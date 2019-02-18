2cf0d5bc976dd13f33bd882ca601bec9916c364e
xantorohara <xantorohara@local>
2019-02-18 23:48:34 +0300

    bits

--- bits.js
+++ bits.js
@@ -1,7 +1,6 @@
 function bits() {
-    const BASE = 64;
-    const BITS = 6;
-    const ZERO = '0'.repeat(BITS);
+    const BASE = 6;
+    const ZERO = '0'.repeat(BASE);
     const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!';
 
     const charsMap = chars.split('').reduce(function (map, char, index) {
@@ -11,39 +10,70 @@ function bits() {
 
     let data = [];
 
-    function numToChar(uint6) {
-        return chars[uint6];
-    }
-
     function fromBinaryString(str) {
-        //todo validate
-        data = str.split('');
+        data = [];
+        for (let i = 0; i < str.length; i++) {
+            const char = str.charAt(i);
+            if (char === '0' && char === '1') {
+                data.push(char);
+            } else {
+                throw 'Invalid char: ' + char + ' in the ' + str;
+            }
+        }
     }
 
     function fromCompressedString(str) {
         const result = [];
         for (let i = 0; i < str.length; i++) {
-            const chr = charsMap[str.charAt(i)];
-            if (chr === undefined) {
-                throw 'Invalid char: ' + chr + ' in the ' + str;
+            const char = str.charAt(i);
+            const byte = charsMap[char];
+            if (byte === undefined) {
+                throw 'Invalid char: ' + char + ' in the ' + str;
+            } else {
+                result.push((ZERO + byte.toString(2)).substr(-BASE));
             }
-            result.push((ZERO + chr.toString(2)).substr(-BITS));
         }
         fromBinaryString(result.join(''))
     }
 
     function toCompressedString() {
-        const chunksCount = Math.ceil(data.length / BITS);
+        const chunksCount = Math.ceil(data.length / BASE);
         const result = [];
         for (let i = 0; i < chunksCount; i++) {
-            const num = parseInt(data.slice(i * BITS, (i + 1) * BITS).join(''), 2);
-            result.push(numToChar(num));
+            const num = parseInt(data.slice(i * BASE, (i + 1) * BASE).join(''), 2);
+            result.push(chars[num]);
         }
-
         return result.join('').replace(/0+$/, '');
     }
 
+    function getBit(index) {
+        if (data.length > index) {
+            return data[index];
+        } else {
+            throw 'Invalid index: ' + index;
+        }
+    }
+
+    function setBit0(index) {
+        if (data.length > index) {
+            data[index] = 0;
+        } else {
+            throw 'Invalid index: ' + index;
+        }
+    }
+
+    function setBit1(index) {
+        if (data.length > index) {
+            data[index] = 1;
+        } else {
+            throw 'Invalid index: ' + index;
+        }
+    }
+
     return {
+        getBit: getBit,
+        setBit0: setBit0,
+        setBit1: setBit1,
         fromBinaryString: fromBinaryString,
         fromCompressedString: fromCompressedString,
         toCompressedString: toCompressedString
--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 $(function () {
-    var $matrix = $('#matrix');
+    const $matrix = $('#matrix');
+    const bits = bits();
 
     function generateMatrix(rowsCount, colsCount) {
-        var out = [];
-        for (var i = 0; i <= rowsCount; i++) {
+        const out = [];
+        for (let i = 0; i <= rowsCount; i++) {
             out.push('<tr>');
-            for (var j = 0; j <= colsCount; j++) {
+            for (let j = 0; j <= colsCount; j++) {
                 if (i === 0) {
                     if (j === 0) {
                         out.push('<td class="num" data-row="' + i + '" data-col="' + j + '">X</td>');
@@ -31,7 +32,31 @@ $(function () {
     $matrix.find('.led').mousedown(function () {
         console.log(this);
         $(this).toggleClass('active');
+
     });
 
+    function framesToPatterns() {
+        const out = [];
+        // $frames.find('.frame').each(function () {
+        //     out.push($(this).attr('data-hex'));
+        // });
+        // return out;
+    }
+
+
+    function saveState() {
+        var patterns = framesToPatterns();
+        // printArduinoCode(patterns);
+        window.location.hash = savedHashState = patterns.join('|');
+    }
+
+    let savedHashState;
+    $(window).on('hashchange', function () {
+        if (window.location.hash.slice(1) !== savedHashState) {
+            console.log(savedHashState);
+        }
+    });
 
 });
+
+
